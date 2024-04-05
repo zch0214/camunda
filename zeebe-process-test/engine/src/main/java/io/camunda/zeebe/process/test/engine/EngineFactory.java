d6385df599343dfefd3691f24c7bc4e35746464c
Philipp Ossler <philipp.ossler@gmail.com>
2024-04-05 06:09:54 +0200

    feat: Move ZPT project into Zeebe

---
+++ zeebe-process-test/engine/src/main/java/io/camunda/zeebe/process/test/engine/EngineFactory.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.process.test.engine;
+
+import io.camunda.zeebe.db.ZeebeDb;
+import io.camunda.zeebe.engine.Engine;
+import io.camunda.zeebe.engine.EngineConfiguration;
+import io.camunda.zeebe.engine.processing.EngineProcessors;
+import io.camunda.zeebe.engine.processing.message.command.SubscriptionCommandSender;
+import io.camunda.zeebe.engine.processing.streamprocessor.JobStreamer;
+import io.camunda.zeebe.logstreams.log.LogStream;
+import io.camunda.zeebe.logstreams.log.LogStreamBuilder;
+import io.camunda.zeebe.logstreams.log.LogStreamReader;
+import io.camunda.zeebe.logstreams.storage.LogStorage;
+import io.camunda.zeebe.process.test.api.ZeebeTestEngine;
+import io.camunda.zeebe.process.test.engine.db.InMemoryDbFactory;
+import io.camunda.zeebe.protocol.ZbColumnFamilies;
+import io.camunda.zeebe.protocol.record.intent.Intent;
+import io.camunda.zeebe.scheduler.Actor;
+import io.camunda.zeebe.scheduler.ActorScheduler;
+import io.camunda.zeebe.scheduler.ActorSchedulingService;
+import io.camunda.zeebe.scheduler.clock.ActorClock;
+import io.camunda.zeebe.scheduler.clock.ControlledActorClock;
+import io.camunda.zeebe.stream.impl.StreamProcessor;
+import io.camunda.zeebe.util.FeatureFlags;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+public class EngineFactory {
+
+  public static ZeebeTestEngine create() {
+    return create(findFreePort());
+  }
+
+  private static int findFreePort() {
+    final int freePort;
+    try (final var serverSocket = new ServerSocket(0)) {
+      freePort = serverSocket.getLocalPort();
+    } catch (final IOException e) {
+      throw new RuntimeException(e);
+    }
+    return freePort;
+  }
+
+  public static ZeebeTestEngine create(final Consumer<Intent> requestListener) {
+    return create(findFreePort(), requestListener);
+  }
+
+  public static ZeebeTestEngine create(final int port) {
+    return create(port, null);
+  }
+
+  private static ZeebeTestEngine create(final int port, final Consumer<Intent> requestListener) {
+    final int partitionId = 1;
+    final int partitionCount = 1;
+
+    final ControlledActorClock clock = createActorClock();
+    final ActorScheduler scheduler = createAndStartActorScheduler(clock);
+
+    final InMemoryLogStorage logStorage = new InMemoryLogStorage();
+    final LogStream logStream = createLogStream(logStorage, scheduler, partitionId);
+
+    final CommandWriter commandWriter = new CommandWriter(logStream.newLogStreamWriter().join());
+    final CommandSender commandSender = new CommandSender(commandWriter);
+    final GatewayRequestStore gatewayRequestStore = new GatewayRequestStore();
+    final InMemoryJobStreamer jobStreamer = new InMemoryJobStreamer(commandWriter);
+    final GrpcToLogStreamGateway gateway =
+        new GrpcToLogStreamGateway(
+            commandWriter, partitionId, partitionCount, port, gatewayRequestStore, jobStreamer);
+    final Server grpcServer = ServerBuilder.forPort(port).addService(gateway).build();
+
+    final GrpcResponseWriter grpcResponseWriter =
+        new GrpcResponseWriter(gateway, gatewayRequestStore, requestListener);
+
+    final ZeebeDb<ZbColumnFamilies> zeebeDb = createDatabase();
+
+    final StreamProcessor streamProcessor =
+        createStreamProcessor(
+            logStream,
+            zeebeDb,
+            scheduler,
+            grpcResponseWriter,
+            partitionCount,
+            commandSender,
+            jobStreamer);
+
+    final EngineStateMonitor engineStateMonitor =
+        new EngineStateMonitor(logStorage, streamProcessor);
+
+    final LogStreamReader reader = logStream.newLogStreamReader().join();
+    final RecordStreamSourceImpl recordStream = new RecordStreamSourceImpl(reader, partitionId);
+
+    return new InMemoryEngine(
+        grpcServer,
+        streamProcessor,
+        gateway,
+        zeebeDb,
+        logStream,
+        scheduler,
+        recordStream,
+        clock,
+        engineStateMonitor);
+  }
+
+  private static ControlledActorClock createActorClock() {
+    return new ControlledActorClock();
+  }
+
+  private static ActorScheduler createAndStartActorScheduler(final ActorClock clock) {
+    final ActorScheduler scheduler =
+        ActorScheduler.newActorScheduler().setActorClock(clock).build();
+    scheduler.start();
+    return scheduler;
+  }
+
+  private static LogStream createLogStream(
+      final LogStorage logStorage, final ActorSchedulingService scheduler, final int partitionId) {
+    final LogStreamBuilder builder =
+        LogStream.builder()
+            .withPartitionId(partitionId)
+            .withLogStorage(logStorage)
+            .withActorSchedulingService(scheduler);
+
+    final CompletableFuture<LogStream> theFuture = new CompletableFuture<>();
+
+    scheduler.submitActor(
+        Actor.wrap(
+            (control) ->
+                builder
+                    .buildAsync()
+                    .onComplete(
+                        (logStream, failure) -> {
+                          if (failure != null) {
+                            theFuture.completeExceptionally(failure);
+                          } else {
+                            theFuture.complete(logStream);
+                          }
+                        })));
+
+    return theFuture.join();
+  }
+
+  private static ZeebeDb<ZbColumnFamilies> createDatabase() {
+    final InMemoryDbFactory<ZbColumnFamilies> factory = new InMemoryDbFactory<>();
+    return factory.createDb();
+  }
+
+  private static StreamProcessor createStreamProcessor(
+      final LogStream logStream,
+      final ZeebeDb<ZbColumnFamilies> database,
+      final ActorSchedulingService scheduler,
+      final GrpcResponseWriter grpcResponseWriter,
+      final int partitionCount,
+      final CommandSender commandSender,
+      final JobStreamer jobStreamer) {
+    return StreamProcessor.builder()
+        .logStream(logStream)
+        .zeebeDb(database)
+        .commandResponseWriter(grpcResponseWriter)
+        .partitionCommandSender(commandSender)
+        .recordProcessors(
+            List.of(
+                new Engine(
+                    context ->
+                        EngineProcessors.createEngineProcessors(
+                            context,
+                            partitionCount,
+                            new SubscriptionCommandSender(context.getPartitionId(), commandSender),
+                            commandSender,
+                            FeatureFlags.createDefault(),
+                            jobStreamer),
+                    new EngineConfiguration())))
+        .actorSchedulingService(scheduler)
+        .build();
+  }
+}
---
+++ zeebe-process-test/filters/src/test/java/io/camunda/zeebe/process/test/filters/logger/RecordStreamLoggerTest.java
@@ -0,0 +1,214 @@
+/*
+ * Copyright © 2021 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.zeebe.process.test.filters.logger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.zeebe.protocol.record.ImmutableRecord;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.RecordType;
+import io.camunda.zeebe.protocol.record.ValueType;
+import io.camunda.zeebe.protocol.record.intent.JobIntent;
+import io.camunda.zeebe.protocol.record.intent.ProcessInstanceCreationIntent;
+import io.camunda.zeebe.protocol.record.intent.ProcessInstanceMigrationIntent;
+import io.camunda.zeebe.protocol.record.value.ImmutableJobRecordValue;
+import io.camunda.zeebe.protocol.record.value.ImmutableProcessInstanceCreationRecordValue;
+import io.camunda.zeebe.protocol.record.value.ImmutableProcessInstanceCreationStartInstructionValue;
+import io.camunda.zeebe.protocol.record.value.ImmutableProcessInstanceMigrationMappingInstructionValue;
+import io.camunda.zeebe.protocol.record.value.ImmutableProcessInstanceMigrationRecordValue;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Named;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class RecordStreamLoggerTest {
+
+  private static final Map<String, Object> TYPED_TEST_VARIABLES = new HashMap<>();
+
+  static {
+    TYPED_TEST_VARIABLES.put("stringProperty", "stringValue");
+    TYPED_TEST_VARIABLES.put("numberProperty", 123);
+    TYPED_TEST_VARIABLES.put("booleanProperty", true);
+    TYPED_TEST_VARIABLES.put("complexProperty", Arrays.asList("Element 1", "Element 2"));
+    TYPED_TEST_VARIABLES.put("nullProperty", null);
+  }
+
+  @Test
+  void testAllValueTypesAreMapped() {
+    final Map<ValueType, Function<Record<?>, String>> valueTypeLoggers =
+        new RecordStreamLogger(null).getValueTypeLoggers();
+
+    final SoftAssertions softly = new SoftAssertions();
+    Arrays.asList(ValueType.values())
+        .forEach(
+            valueType ->
+                softly
+                    .assertThat(valueTypeLoggers.containsKey(valueType))
+                    .withFailMessage("No value type logger defined for value type '%s'", valueType)
+                    .isTrue());
+
+    softly.assertAll();
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("provideVariables")
+  void testLogVariable(final String key, final Object value) {
+    final RecordStreamLogger logger = new RecordStreamLogger(null);
+
+    final String result = logger.logVariables(Collections.singletonMap(key, value));
+
+    assertThat(result).isEqualTo(String.format("(Variables: [%s -> %s])", key, value));
+  }
+
+  @Test
+  void testLogMultipleVariables() {
+    final RecordStreamLogger logger = new RecordStreamLogger(null);
+
+    final String result = logger.logVariables(TYPED_TEST_VARIABLES);
+
+    assertThat(result)
+        .contains("Variables: ")
+        .contains("stringProperty -> stringValue")
+        .contains("numberProperty -> 123")
+        .contains("booleanProperty -> true")
+        .contains("complexProperty -> [Element 1, Element 2]")
+        .contains("nullProperty -> null");
+  }
+
+  @ParameterizedTest(name = "logged record {0} should contain {1}")
+  @MethodSource("loggedRecordContains")
+  void testLoggedRecordContains(final Record<?> typedRecord, final String expected) {
+    final RecordStreamLogger logger = new RecordStreamLogger(null);
+    final String result = logger.logRecord(typedRecord);
+    assertThat(result).contains(expected);
+  }
+
+  private static Stream<Arguments> provideVariables() {
+    return TYPED_TEST_VARIABLES.entrySet().stream()
+        .map(entry -> Arguments.of(entry.getKey(), entry.getValue()));
+  }
+
+  private static Stream<Arguments> loggedRecordContains() {
+    return Stream.of(
+        Arguments.of(
+            Named.of(
+                "PROCESS_INSTANCE_CREATION starting at default none start event",
+                ImmutableRecord.builder()
+                    .withRecordType(RecordType.EVENT)
+                    .withValueType(ValueType.PROCESS_INSTANCE_CREATION)
+                    .withIntent(ProcessInstanceCreationIntent.CREATED)
+                    .withKey(123)
+                    .withValue(
+                        ImmutableProcessInstanceCreationRecordValue.builder()
+                            .withBpmnProcessId("PROCESS")
+                            .withVersion(1)
+                            .build())
+                    .build()),
+            "(Process id: PROCESS), (default start)"),
+        Arguments.of(
+            Named.of(
+                "PROCESS_INSTANCE_CREATION starting at default none start event with variables",
+                ImmutableRecord.builder()
+                    .withRecordType(RecordType.EVENT)
+                    .withValueType(ValueType.PROCESS_INSTANCE_CREATION)
+                    .withIntent(ProcessInstanceCreationIntent.CREATED)
+                    .withKey(123)
+                    .withValue(
+                        ImmutableProcessInstanceCreationRecordValue.builder()
+                            .withBpmnProcessId("PROCESS")
+                            .withVersion(1)
+                            .withVariables(
+                                new HashMap<String, Object>() {
+                                  {
+                                    put("key", "value");
+                                  }
+                                })
+                            .build())
+                    .build()),
+            "(Process id: PROCESS), (Variables: [key -> value]), (default start)"),
+        Arguments.of(
+            Named.of(
+                "PROCESS_INSTANCE_CREATEION starting at given elements",
+                ImmutableRecord.builder()
+                    .withRecordType(RecordType.EVENT)
+                    .withValueType(ValueType.PROCESS_INSTANCE_CREATION)
+                    .withIntent(ProcessInstanceCreationIntent.CREATED)
+                    .withKey(123)
+                    .withValue(
+                        ImmutableProcessInstanceCreationRecordValue.builder()
+                            .withBpmnProcessId("PROCESS")
+                            .withVersion(1)
+                            .addStartInstruction(
+                                ImmutableProcessInstanceCreationStartInstructionValue.builder()
+                                    .withElementId("USER_TASK")
+                                    .build())
+                            .addStartInstruction(
+                                ImmutableProcessInstanceCreationStartInstructionValue.builder()
+                                    .withElementId("SERVICE_TASK")
+                                    .build())
+                            .build())
+                    .build()),
+            "(Process id: PROCESS), (starting before elements: USER_TASK, SERVICE_TASK)"),
+        Arguments.of(
+            Named.of(
+                "JOB with element id and type",
+                ImmutableRecord.builder()
+                    .withRecordType(RecordType.EVENT)
+                    .withValueType(ValueType.JOB)
+                    .withIntent(JobIntent.COMPLETED)
+                    .withKey(123)
+                    .withValue(
+                        ImmutableJobRecordValue.builder()
+                            .withType("task")
+                            .withElementId("serviceTask1")
+                            .build())
+                    .build()),
+            "(Element id: serviceTask1), (Job type: task)"),
+        Arguments.of(
+            Named.of(
+                "Process instance migration with mapping instructions",
+                ImmutableRecord.builder()
+                    .withRecordType(RecordType.EVENT)
+                    .withValueType(ValueType.PROCESS_INSTANCE_MIGRATION)
+                    .withIntent(ProcessInstanceMigrationIntent.MIGRATED)
+                    .withKey(123)
+                    .withValue(
+                        ImmutableProcessInstanceMigrationRecordValue.builder()
+                            .withProcessInstanceKey(123)
+                            .withTargetProcessDefinitionKey(456)
+                            .addMappingInstruction(
+                                ImmutableProcessInstanceMigrationMappingInstructionValue.builder()
+                                    .withSourceElementId("A")
+                                    .withTargetElementId("A")
+                                    .build())
+                            .addMappingInstruction(
+                                ImmutableProcessInstanceMigrationMappingInstructionValue.builder()
+                                    .withSourceElementId("B")
+                                    .withTargetElementId("C")
+                                    .build())
+                            .build())
+                    .build()),
+            "(Process instance key: 123), (Target process definition key: 456), (Mapping instructions: A -> A, B -> C)"));
+  }
+}
